a4507bb114656a38e88b6a54aa5841357d33e5d2
ManuelMannhold <M.Mannhold@gmx.de>
2026-01-26 12:56:29 +0100

    add styling

---
+++ src/app/features/dashboard/components/create-note-dialog/create-note-dialog.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogActions, MatDialogClose, MatDialogContent } from "@angular/material/dialog";
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+
+@Component({
+  selector: 'app-create-note-dialog',
+  imports: [MatDialogContent, MatFormField, MatLabel, MatInput, MatDialogActions, MatDialogClose, MatButtonModule],
+  templateUrl: './create-note-dialog.html',
+  styleUrl: './create-note-dialog.scss',
+})
+export class CreateNoteDialog {
+
+}
--- src/app/features/dashboard/sidebar/sidebar/sidebar.ts
+++ src/app/features/dashboard/sidebar/sidebar/sidebar.ts
@@ -1,11 +1,23 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from "@angular/material/icon";
+import { CreateNoteDialog } from '../../components/create-note-dialog/create-note-dialog';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
 export class Sidebar {
 
+  constructor(private dialog: MatDialog) {}
+
+openCreateNoteDialog() {
+  this.dialog.open(CreateNoteDialog, {
+    width: '450px',
+    panelClass: 'custom-glass-dialog',
+    data: { /* hier könntest du die aktuelle Ordner-ID übergeben */ }
+  });
+}
 }
