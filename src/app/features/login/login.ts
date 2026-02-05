163939b6e6abc749bc76fc510fda48957dfd7678
ManuelMannhold <M.Mannhold@gmx.de>
2026-02-05 09:52:19 +0100

    add user registration / set errors

--- src/app/features/login/login.ts
+++ src/app/features/login/login.ts
@@ -1,40 +1,128 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { MatButton } from '@angular/material/button';
-import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatIcon } from '@angular/material/icon';
 import { MatInput } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { NgParticlesService, NgxParticlesModule } from "@tsparticles/angular";
 import { loadFull } from "tsparticles";
 import { RecursivePartial, IOptions } from "@tsparticles/engine";
-import { Auth, signInAnonymously } from '@angular/fire/auth';
 import { AuthService } from '../../core/services/auth/auth.service';
 import { FormsModule } from '@angular/forms';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { Auth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from '@angular/fire/auth';
+import { ChangeDetectorRef, AfterViewInit } from '@angular/core';
 
 
 @Component({
   selector: 'app-login',
-  imports: [CommonModule, MatFormField, MatIcon, MatLabel, MatButton, MatInput, NgxParticlesModule, FormsModule, MatProgressSpinnerModule],
+  imports: [CommonModule, MatFormField, MatIcon, MatLabel, MatButton, MatInput, NgxParticlesModule, FormsModule, MatProgressSpinnerModule, ReactiveFormsModule, MatError],
   templateUrl: './login.html',
   styleUrl: './login.scss',
 })
 export class Login {
+  private cdr = inject(ChangeDetectorRef);
   private authService = inject(AuthService);
   private router = inject(Router);
 
+
+  authForm!: FormGroup
   email = signal('');
   password = signal('');
   isLoading = signal(false);
   errorMessage = signal('');
+  isRegisterMode = false;
 
-  constructor(private readonly particlesService: NgParticlesService, private auth: Auth) { }
+  constructor(private readonly particlesService: NgParticlesService, private auth: Auth, private fb: FormBuilder) { }
 
   ngOnInit() {
     this.particlesService.init(async (engine) => {
       await loadFull(engine);
     });
+    this.authForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      confirmPassword: ['']
+    });
+    this.authForm.valueChanges.subscribe(() => {
+      if (this.errorMessage()) {
+        this.errorMessage.set('');
+      }
+    });
+  }
+
+  ngAfterViewInit() {
+    this.cdr.detectChanges();
+  }
+
+  async onSubmit() {
+    if (this.authForm.invalid) return;
+
+    const { email, password, confirmPassword } = this.authForm.value;
+    this.isLoading.set(true);
+    this.errorMessage.set('');
+
+    try {
+      if (this.isRegisterMode) {
+        if (password !== confirmPassword) {
+          this.errorMessage.set('Passwörter stimmen nicht überein!');
+          this.isLoading.set(false);
+          return;
+        }
+        await createUserWithEmailAndPassword(this.auth, email, password);
+        this.router.navigate(['/dashboard']);
+      } else {
+        await signInWithEmailAndPassword(this.auth, email, password);
+        this.router.navigate(['/dashboard']);
+      }
+    } catch (error: any) {
+      this.handleAuthError(error);
+    } finally {
+      this.isLoading.set(false);
+    }
+  }
+
+  private handleAuthError(error: any) {
+    console.error('Firebase Error Code:', error.code);
+
+    switch (error.code) {
+      case 'auth/invalid-credential':
+        this.errorMessage.set('E-Mail oder Passwort ist falsch.');
+        break;
+      case 'auth/user-not-found':
+        this.errorMessage.set('Kein Account mit dieser E-Mail gefunden.');
+        break;
+      case 'auth/wrong-password':
+        this.errorMessage.set('Das eingegebene Passwort ist nicht korrekt.');
+        break;
+      case 'auth/email-already-in-use':
+        this.errorMessage.set('Diese E-Mail wird bereits verwendet.');
+        break;
+      default:
+        this.errorMessage.set('Ein Fehler ist aufgetreten. Bitte versuche es nochmal.');
+    }
+    setTimeout(() => {
+      this.errorMessage.set('');
+    }, 5000);
+  }
+
+  toggleRegisterMode() {
+    this.isRegisterMode = !this.isRegisterMode;
+    this.errorMessage.set('');
+    this.authForm.get('email')?.reset();
+    this.authForm.get('password')?.reset();
+    this.authForm.get('confirmPassword')?.reset();
+    const confirmControl = this.authForm.get('confirmPassword');
+
+    if (this.isRegisterMode) {
+      confirmControl?.setValidators([Validators.required]);
+    } else {
+      confirmControl?.clearValidators();
+    }
+    confirmControl?.updateValueAndValidity();
+    this.cdr.detectChanges();
   }
 
   async particlesLoaded(): Promise<void> {
